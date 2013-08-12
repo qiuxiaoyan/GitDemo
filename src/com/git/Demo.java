a0afa10af622071228a97defac6003855aa611cc
qiuxiaoyan <qiuxiaoyan@hikvision.com.cn>
2013-08-12 17:11:51 +0800

    t
    Signed-off-by: qiuxiaoyan <qiuxiaoyan@hikvision.com.cn>

---
+++ src/com/git/Demo.java
@@ -0,0 +1,16 @@
+package com.git;
+/**
+ * Copyright: Copyright (c) 2012-2014
+ * Company: Hikvision(http://www.hikvision.com)
+ * @author：qiuxiaoyan
+ * @Date:   2013-8-12 下午4:19:34
+ * @describe：
+ */
+public class Demo {
+	private static String msg = "The first git demo";
+	
+	public static void main(){
+		System.out.println(msg);
+		System.out.println("Hello World");
+	}
+}
